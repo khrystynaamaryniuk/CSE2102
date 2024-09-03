258fe770eff7587f33ffcfe3094d3bbef2ddb8fa
khrystynaamaryniuk <khrystyna.maryniuk@uconn.edu>
2024-09-03 14:23:24 -0400

    init

---
+++ Hello.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Hello here.
+ *
+ * @author (Khrystyna Maryniuk)
+ * @version (9/1/2021)
+ */
+public class Hello
+{
+    public static void main(String [] args) {
+        // \t - tab
+        // \n - new line
+        // \\ - prints a backslash
+        // \" - prints "
+        // \r - carriage return 
+        // \' - prints '
+        // \f - clears return 
+        System.out.print("Hello, \n world!");
+        System.out.println("Hello, ");
+        System.out.println("world");
+        
+    }
+}
